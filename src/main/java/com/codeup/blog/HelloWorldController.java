ad4bf7bf16f061703bc45095796ec395cf4d179a
Fernando Mendoza R <fernandoza@gmail.com>
2019-05-01 15:03:46 -0500

    feat: add the controllers lecture

---
+++ src/main/java/com/codeup/blog/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.codeup.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello from spring";
+    }
+//
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHelloWithName(@PathVariable String name){
+        return "Hello from " + name;
+    }
+
+    @PostMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHelloWithName(){
+        return "POST mapping works";
+    }
+
+//    @RequestMapping(path = "/hello/{name}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String sayHelloWithName(@PathVariable String name){
+//        return "Hello from " + name;
+//    }
+
+
+
+}
